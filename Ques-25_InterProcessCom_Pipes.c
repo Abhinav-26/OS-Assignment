f7f24787a1f22f56b9337ba27c7615a1b1ca9ddb
Abhinav Dubey <abhinavdubey616@gmail.com>
2020-04-11 15:41:52 +0530

    InterProcess Communication with Pipes

--- Ques-25_InterProcessCom_Pipes.c
+++ Ques-25_InterProcessCom_Pipes.c
@@ -5,17 +5,49 @@
 #include<sys/types.h>
 int main()
 {
+	system("clear");
 	char str[100];
+	system("tput setaf 2");
+	int n;    		 
+ 	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	printf("Please enter length of Characters of Your String :");
+	scanf("%d", &n);
+ 	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+	system("tput setaf 7");
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	system("tput setaf 4");
 	printf("Enter your String :");
 	int i=0;
-	while(i<15){
+
+	while(i<n)
+	{
 	scanf("%c",&str[i]);
-	i++;}
+	i++;
+	}
+	system("tput setaf 7");
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");	
+
 	int fd[2];	
 	write(fd[1],str,strlen(str));
-	printf("Pid for Writing in Pipe is %d- \n",getpid());
+	
+	
+	system("tput setaf 7");
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	system("tput setaf 3");
+	printf("PID for Writing in Pipe is %d- \n",getpid());
+	system("tput setaf 7");
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+
 	int y=strlen(str);
-	printf("We have Written in pipe :%s\n",str);	
+	
+	system("tput setaf 7");
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	system("tput setaf 5");
+	printf("We have Written in pipe :%s",str);	
+	system("tput setaf 7");
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+
 	for(i=0;i<strlen(str);i++)
 		{
 		if((int)str[i]>=65 && (int)str[i]<=90)
@@ -31,7 +63,17 @@ int main()
 		}
 	close(fd[0]);
 	read(fd[0],str,strlen(str));
-	printf("\n\nPid for reading from Pipe is %d \n",getpid());
-	printf("Reading from the file  :%s\n",str);
+
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+	system("tput setaf 6");
+	printf("\nPid for reading from Pipe is %d \n",getpid());
+	system("tput setaf 6");
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+
+	system("tput setaf 3");
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	printf("Reading from the file  :%s",str);
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	system("tput setaf 7");
 	
 }
